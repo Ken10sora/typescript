2e8ab852a896b1fea16be4a84122084d640a6c89
Ken10 <76900596+Ken10sora@users.noreply.github.com>
2024-05-26 17:57:16 +0900

    Add files via upload

---
+++ assertion.test.ts
@@ -0,0 +1,31 @@
+import { ciBuild, User } from "./assertion"
+
+test('build state should be success', () => {
+  expect(ciBuild).toHaveProperty('state', 'success')
+})
+
+test('actor should be Taka', () => {
+  expect(ciBuild).toHaveProperty('actor.login', 'Taka')
+})
+
+test('triggered by the scheduled pipeline', () => {
+  expect(ciBuild).toEqual(
+    expect.objectContaining({
+      triggerParameters: expect.objectContaining({ is_scheduled: true }),
+      type: 'scheduled_pipeline',
+    }),
+  )
+})
+
+test('creates a new user with a 6-character password', () => {
+  expect(new User({ name: 'hoge', password: '123456' })).toEqual({
+    name: 'hoge',
+    password: '123456',
+  })
+})
+
+test('throw Error when the length of password is less than 6', () => {
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow()
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(Error)
+  expect(() => new User ({ name: 'hoge', password: '12345' })).toThrow('The password length must be at least 6 characters.',)
+})
---
+++ assertion.ts
@@ -0,0 +1,12 @@
+export const ciBuild = {
+  number: 1,
+  duration: 12000,
+  state: 'success',
+  triggerParameters: {
+    is_scheduled: true,
+  },
+  type: 'scheduled_pipeline',
+  actor: {
+    login: 'Taka',
+  },
+}
