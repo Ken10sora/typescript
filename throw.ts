87bb6055891058fdfd3f14d7997559dc4f51a766
Ken10 <76900596+Ken10sora@users.noreply.github.com>
2024-05-26 18:40:04 +0900

    Delete throw.ts

--- throw.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  name: string
-  password: string
-  constructor({ name, password }: { name: string, password: string}) {
-    if(password.length < 6) throw new Error('The password length must be at least 6 characters.')
-      this.name = name
-      this.password = password
-  }
-}
